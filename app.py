4fdf6c92919ba524eda068d8e8c9512d752339d6
zhanglun <zhanglun1410@gmail.com>
2014-11-26 16:45:49 +0800

    modified

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, url_for, make_response, redirect, render_template, session, flash
+from datetime import datetime
+from flask.ext.moment import Moment
+
 # from route import *
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+moment = Moment(app)
 
+print moment
 
 @app.route('/')
 def index():
+    print datetime.utcnow()
     return 'Welcome!'
 
 
@@ -32,6 +38,18 @@ def checkuser():
             print error
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', now = datetime.utcnow()), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 
 if __name__ == '__main__':
