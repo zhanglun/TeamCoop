ae9700d80bee385caf2a912b5121778aca0b623d
fanbaizhou <fbzhh007@gmail.com>
2014-11-28 16:25:52 +0800

    add datebase dev files

---
+++ db/dev/table_project.sql
@@ -0,0 +1,10 @@
+create table project (id integer primary key autoincrement,
+					  title text not null,
+					  description text not null,
+					  level integer not null default 1,
+					  deadline date not null default (datetime(current_date,'localtime')),
+					  status integer not null default 1,
+					  isPublic integer not null default 1,
+					  createUserId integer not null,
+					  createtime timestamp not null default (datetime(current_timestamp,'localtime')),
+					  unique(title));
