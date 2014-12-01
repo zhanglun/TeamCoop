1e0a7a1df87f83e0c0f748bb3daaf06e621a6e22
zhanglun <zhanglun1410@gmail.com>
2014-12-01 16:47:48 +0800

    添加成员

--- teamcoop/models/user.py
+++ teamcoop/models/user.py
@@ -1,15 +1,17 @@
 from __init__ import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
+    password = db.Column(db.Text, default='123456')
     name = db.Column(db.Text, default='')
     gender = db.Column(db.Text, default='')
     phone = db.Column(db.Text, default='')
     email = db.Column(db.Text, default='')
-    createtime = db.Column(db.DateTime, default=time.time()),
+    createtime = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,6 +19,7 @@ class User(db.Model):
 
 class UserDepartMent(db.Model):
     __tablename__ = 'user_department'
+    id = db.Column(db.Integer, primary_key=True)
     departmentId = db.Column(db.Integer, nullable=False)
     userId = db.Column(db.Text, nullable=False)
 
@@ -26,6 +29,7 @@ class UserDepartMent(db.Model):
 
 class UserProject(db.Model):
     __tablename__ = 'user_project'
+    id = db.Column(db.Integer, primary_key=True)
     projectId = db.Column(db.Integer, nullable=False)
     userId = db.Column(db.Integer, nullable=False)
     level = db.Column(db.Integer, nullable=False, default=2)
--- teamcoop/views/api.py
+++ teamcoop/views/api.py
@@ -1,18 +1,42 @@
+#coding: utf-8
 from flask import Blueprint, render_template
-
 from __init__ import *
 
 api = Blueprint('api', __name__)
 
-@api.route('/add/member/')
-def add_member():
-    pass
+print api_response_dict
 
-@api.route('/add/user/')
-def add_user():
-    print "TeamCoop add_user API"
-    # db.create_all()
-    u = user.User(username='zhanglun', password='213', name='zhangxiaolun', gender='man', phone='18270919495', email='zhanglun1410@gmail.com')
-    db.session.add(u)
-    db.session.commit()
-    return 'add user!'
+@api.route('/project/member/', methods=['GET', 'POST'])
+def set_member():
+    if request.method == 'POST':
+        # get data from request.json
+        username = request.json['username']
+        # checkout if the user is already exist
+        check = user.User.query.filter_by(username=username).first()
+        if check is None:
+            # create a new member
+            new_member = user.User(username=username)
+            db.session.add(new_member)
+            db.session.commit()
+            # set the response
+            api_response_dict['code'] = 'success'
+            api_response_dict['result'] = 'add a new member'
+            response = make_response(jsonify(api_response_dict))
+            response.status = '200'
+        else:
+            response = make_response('already exist')
+            response.status = '200'
+        return response
+
+    elif request.method == 'GET':
+        return "Down!"
+
+@api.route('/project/person/', methods=['GET', 'POST'])
+def set_person():
+    if request.method == 'POST':
+        name = request.json['name']
+        username = request.json['username']
+        password = request.json['password']
+        confirm_password = request.json['confirm_password']
+    elif request.method == 'GET':
+        return 'API-setting person'
