7b970faf13285e5e6ef7a1f21fe4570ba33e82a2
zhanglun <zhanglun1410@gmail.com>
2014-12-01 16:48:35 +0800

    update views

--- teamcoop/views/__init__.py
+++ teamcoop/views/__init__.py
@@ -2,10 +2,12 @@
 # 在 views 的 __init__.py 中导入forms和models
 
 import json
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+import yaml
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session, jsonify, make_response
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from teamcoop.forms import userlogin
 from teamcoop.models import db, user
 
 
+api_response_dict={'code': '', 'result': ''}
