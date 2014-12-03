7e4a079c6ddaa8b7794f6dd1b3079959ecb17645
fanbaizhou <fbzhh007@gmail.com>
2014-12-03 13:24:40 +0800

    update:remove column phone from table user

--- teamcoop/db/dev/table_user.sql
+++ teamcoop/db/dev/table_user.sql
@@ -4,7 +4,5 @@ create table user (id integer primary key unique,
 				   level integer not null default 2,
 				   name text not null default '',
 				   gender text not null default '',
-				   phone text not null default '',
 				   email text not null default '',
-				   createtime timestamp not null default (datetime(current_timestamp,'localtime')),
-				   check(length(phone) > 10));
+				   createtime timestamp not null default (datetime(current_timestamp,'localtime')));
