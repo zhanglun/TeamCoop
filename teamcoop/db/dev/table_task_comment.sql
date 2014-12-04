e5c73239149c0e1f0bda17661d23d5342171d98a
fanbaizhou <fbzhh007@gmail.com>
2014-12-04 16:38:50 +0800

    modified foreign key logic

--- teamcoop/db/dev/table_task_comment.sql
+++ teamcoop/db/dev/table_task_comment.sql
@@ -1,5 +1,5 @@
 create table task_comment (id integer primary key unique,
 						   content text not null default '',
-						   taskId integer not null references project(id) on delete cascade deferrable initially deferred,
-						   userId integer not null references user(id) on delete cascade deferrable initially deferred,
+						   taskId integer not null references project(id) on delete cascade deferrable initially immediate,
+						   userId integer not null references user(id) on delete cascade deferrable initially immediate,
 						   createtime timestamp not null default (datetime(current_timestamp,'localtime')));
--- teamcoop/db/dev/table_user_project.sql
+++ teamcoop/db/dev/table_user_project.sql
@@ -1,5 +1,5 @@
 create table user_project (id integer primary key unique,
-						   projectId integer not null references project(id) on delete cascade deferrable initially deferred,
-						   userId integer not null references user(id) on delete cascade deferrable initially deferred,
+						   projectId integer not null references project(id) on delete cascade deferrable initially immediate,
+						   userId integer not null references user(id) on delete cascade deferrable initially immediate,
 						   level integer not null default 2,
 						   unique(projectId,userId));
