1c1d9e2e0b8a423d7d8063bad5c8deb1d7580a03
mute_zephyr <abortion2012@gmail.com>
2014-12-05 17:44:36 +0800

    update commonjs

--- teamcoop/src/scripts/common.js
+++ teamcoop/src/scripts/common.js
@@ -1,22 +1,43 @@
 var postData = {};
 
-postData.createProject = function(formSelector) {
-    $(formSelector + ' :input').serializeArray();
-    var fields = $('.form-horizontal').serializeArray()
-        // array of form-data
+postData.getData = function(formSelector) {
+    var fields = $(formSelector + ' :input').serializeArray();
+    // var fields = $('.form-horizontal').serializeArray()
+    // array of form-data
     var data = {};
     $.each(fields, function(index, obj) {
         var name = obj.name,
             value = obj.value;
-        console.log('participants:' + data['participants']);
         if (data.hasOwnProperty(name) == false) {
             data[name] = value;
         } else if ($.isArray(data[name]) == true) {
-            console.log(33)
-                // data[name] = data[name].push(value);
+            // update value
+            data[name].push(value);
         } else {
-            data[name] = [value];
+            // new array and push oldvalue
+            var oldvalue = data[name];
+            data[name] = new Array();
+            data[name].push(oldvalue);
+            data[name].push(value);
         }
     });
-    console.log(data);
+    return data;
 }
+
+// confirm input of required 
+$(function(){
+    $(':input').on('blur',function(event){
+
+    });
+});
+
+
+
+
+// setting module part
+var settingModule = {};
+
+// 个人设置
+settingModule.personalSetting = function(){}
+//  admin成员设置
+settingModule.memberSetting = function(){}
