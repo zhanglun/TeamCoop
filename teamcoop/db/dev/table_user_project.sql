4f016ba2675069d4c2b7080ded8c16a56d8dc0b5
fanbaizhou <fbzhh007@gmail.com>
2014-12-08 17:18:13 +0800

    update db: add columns createtime to tables

--- teamcoop/db/dev/table_user_project.sql
+++ teamcoop/db/dev/table_user_project.sql
@@ -2,4 +2,5 @@ create table user_project (id integer primary key unique,
 						   projectId integer not null references project(id) on delete cascade deferrable initially immediate,
 						   userId integer not null references user(id) on delete cascade deferrable initially immediate,
 						   level integer not null default 2,
+						   createtime timestamp not null default (datetime(current_timestamp,'localtime')),
 						   unique(projectId,userId));
