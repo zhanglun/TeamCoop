2aa9415f8c8fc100de2418c09da114168e772f86
fanbaizhou <fbzhh007@gmail.com>
2014-12-11 17:36:04 +0800

    add cloumn projectId to table task

--- teamcoop/db/dev/table_task.sql
+++ teamcoop/db/dev/table_task.sql
@@ -4,5 +4,6 @@ create table task (id integer primary key unique,
 				   deadline timestamp not null default (datetime(current_timestamp,'localtime')),
 				   executeUserId integer not null default 0 references user(id) on delete set default deferrable initially immediate,
 				   createUserId integer not null default 0 references user(id) on delete set default deferrable initially immediate,
+				   projectId integer not null default 0 references project(id) on delete set default deferrable initially immediate,
 				   status integer not null default 1,
 				   createtime timestamp not null default (datetime(current_timestamp,'localtime')));
