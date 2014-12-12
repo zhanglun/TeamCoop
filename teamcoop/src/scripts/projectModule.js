c36c45e5c757bbc7cbe9346ce2d7d252e7ac864e
mute_zephyr <abortion2012@gmail.com>
2014-12-12 21:15:19 +0800

    issue detail init

--- teamcoop/src/scripts/projectModule.js
+++ teamcoop/src/scripts/projectModule.js
@@ -55,18 +55,18 @@ projectModule.personalIssue = function() {
         "project_id": projectModule.projectid,
     };
     postData.getdata('/api/user/project/task/', data, function(json) {
-        console.log(json);
+        // console.log(json);
         var array_create = json['result']['tasks']['create'],
             array_excute = json['result']['tasks']['execute'];
         $('#asignToMe tbody,#asignByMe tbody').empty();
         $.each(array_excute, function(index, obj) {
             var status = obj['status'];
-            $('#asignToMe tbody').append('<tr><td><a href="">' + obj['title'] + '</a></td><td>12</td><td>' + obj['create_user_id'] + '</td><td><select name="" id="" class="form-control"><option value="finished">finished</option><option value="unfinish">unfinish</option><option value="executed">executed</option></select></td><td><button type="button" class="btn btn-danger btn-xs" data-issueid="' + obj['id'] + '">delete</button></td></tr>');
+            $('#asignToMe tbody').append('<tr data-issueid="' + obj['id'] + '"><td><a href="javascript:void(0);">' + obj['title'] + '</a></td><td>12</td><td>' + obj['create_user_id'] + '</td><td><select name="" id="" class="form-control"><option value="finished">finished</option><option value="unfinish">unfinish</option><option value="executed">executed</option></select></td><td><button type="button" class="btn btn-danger btn-xs">delete</button></td></tr>');
             $('#asignToMe tbody tr').last().find('option').eq(status - 1).attr('selected', 'selected');
         });
         $.each(array_create, function(index, obj) {
             var status = obj['status'];
-            $('#asignByMe tbody').append('<tr data-issueid="' + obj['id'] + '"><td><a href="" >' + obj['title'] + '</a></td><td>12</td><td>' + obj['execute_user_id'] + '</td><td><select name="" id="" class="form-control"><option value="finished">finished</option><option value="unfinish">unfinish</option><option value="executed">executed</option></select></td><td><button type="button" class="btn btn-danger btn-xs">delete</button></td></tr>');
+            $('#asignByMe tbody').append('<tr data-issueid="' + obj['id'] + '"><td><a href="javascript:void(0);" >' + obj['title'] + '</a></td><td>12</td><td>' + obj['execute_user_id'] + '</td><td><select name="" id="" class="form-control"><option value="finished">finished</option><option value="unfinish">unfinish</option><option value="executed">executed</option></select></td><td><button type="button" class="btn btn-danger btn-xs">delete</button></td></tr>');
             $('#asignByMe tbody tr').last().find('option').eq(status - 1).attr('selected', 'selected');
         });
 
@@ -83,6 +83,7 @@ projectModule.newIssue = function() {
     if (postData.checkForm('#createIssueForm') == false) {
         postData.postdata('/api/project/task/', data, function(json) {
             // console.log(json);
+            // todo
             $('#createIssue').modal('hide');
         });
     } else {
@@ -90,6 +91,42 @@ projectModule.newIssue = function() {
     }
 }
 
+projectModule.changeIssueStatus = function() {
+    $('#asignToMe,#asignByMe').on('change', 'select', function(event) {
+        var index = $(event.target).find(':selected').index() + 1,
+            issueId = $(event.target).parentsUntil('tbody', 'tr').attr('data-issueid');
+        // todo post and change 
+    });
+}
+
+projectModule.deleteIssue = function() {
+    $('#asignToMe,#asignByMe').on('click', 'button', function(event) {
+        var issueId = $(event.target).parentsUntil('tbody', 'tr').attr('data-issueid'),
+            data = {
+                "project_id": projectModule.projectid,
+                "task_id": issueId
+            };
+        console.log(data);
+        if (postData.confirm('确认删除吗?') == true) {
+            postData.postdata('/api/project/task/trash/', data, function(json) {
+                // todo
+                console.log(json);
+                project.removeIssue(issueId);
+            });
+        } else {
+            return false;
+        }
+    });
+}
+
+projectModule.issueDetail = function(id) {
+    $('#issueDetail').modal('show');
+    // todo
+}
+
+projectModule.removeIssue = function(id) {
+    $('[data-issueid="' + id + '"]').remove();
+}
 
 projectModule.resetModal = function() {
     $('.modal').on('hidden.bs.modal', function() {
@@ -109,8 +146,17 @@ projectModule.resetModal = function() {
 $(function() {
     projectModule.personalIssue();
     projectModule.resetModal();
+    // delete issue event bind
+    projectModule.deleteIssue();
+    // select event bind
+    projectModule.changeIssueStatus();
     // new issue event bind
     $('#btnCreateIssue').on('click', projectModule.newIssue);
+    // issue detail 
+    $('#asignToMe,#asignByMe').on('click', 'a', function(event) {
+        var issueid = $(event.target).parentsUntil('tbody', 'tr').attr('data-issueid');
+        projectModule.issueDetail(issueid);
+    });
     // new project btn event bind
     $('#project_btn').on('click', projectModule.newProject);
 });
